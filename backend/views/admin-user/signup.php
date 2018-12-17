41e09f2cbd071363bfa8f851081682c5e08c2155
winvon <win0.von@gmail.com>
2018-12-17 22:15:15 +0800

    '111'

--- backend/views/admin-user/signup.php
+++ backend/views/admin-user/signup.php
@@ -59,7 +59,7 @@ $this->params['breadcrumbs'][] = $this->title;
             top: 2px;
             right: 1px;
         }
-        .intl-tel-input .country-list{
+        .intl-tel-input .country-list {
             z-index: 100;
         }
     </style>
